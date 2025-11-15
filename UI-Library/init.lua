8da2bac994b330b974d9e39ae53ba54dcd922582
muhaaskr537-pixel <muhaaskr537@gmail.com>
2025-11-15 12:34:25 +0300

    Update init.lua

--- UI-Library/init.lua
+++ UI-Library/init.lua
@@ -1,8 +1,8 @@
 local Library = {}
 local Loader = loadstring(game:HttpGet("https://raw.githubusercontent.com/muhaaskr537-pixel/UI-Library/main/UI-Library/Loader.lua"))()
 
-function Library:CreateWindow(title, theme)
-    return Loader.Window.new(title, theme)
+function Library:CreateWindow(title)
+    return Loader.Window:Create(title)
 end
 
 return Library
