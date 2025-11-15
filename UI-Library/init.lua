cb771451905dca599dfd4e70b251e0f66df50232
muhaaskr537-pixel <muhaaskr537@gmail.com>
2025-11-15 12:32:11 +0300

    Refactor Library to integrate Loader for windows
    Updated Library to use Loader for window creation with theme support.

--- UI-Library/init.lua
+++ UI-Library/init.lua
@@ -1,7 +1,8 @@
 local Library = {}
+local Loader = loadstring(game:HttpGet("https://raw.githubusercontent.com/muhaaskr537-pixel/UI-Library/main/UI-Library/Loader.lua"))()
 
-function Library:CreateWindow(title)
-    return require(script.Core.Window).new(title)
+function Library:CreateWindow(title, theme)
+    return Loader.Window.new(title, theme)
 end
 
 return Library
